799d633ef4971efe6b66ea7201e86652e4465db7
Obaloluwa123 <oolaniran3586@myasu.alasu.edu>
2023-03-20 21:23:57 -0400

    .

--- hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -1,7 +1,11 @@
 package com.company.hellocircle.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -11,15 +15,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloCircleController.class)
 public class HelloCircleControllerTest {
 
-    @Mock
+   @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnAnswer() throws Exception {
         // arrange and act
-        mockMvc.perform(get("/answer"))
+       mockMvc.perform(get("/answer"))
                 .andDo(print())
                 .andExpect(status().isOk());
                 // assert
