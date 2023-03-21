1a7ff4d3d51c277f0f924a7720b2bee949ff7832
Obaloluwa123 <oolaniran3586@myasu.alasu.edu>
2023-03-20 21:16:17 -0400

    .

--- hello-circle/src/test/java/com/company/hellocircle/HelloCircleApplicationTests.java
+++ hello-circle/src/test/java/com/company/hellocircle/HelloCircleApplicationTests.java
@@ -1,19 +1,11 @@
 package com.company.hellocircle;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class HelloCircleApplicationTests {
-
 
 	@Test
 	void contextLoads() {
